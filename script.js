83f5992f6b3cac633ffbc21cab58e848f4c97176
mawet <ra78161@uem.br>
2024-05-06 23:08:55 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const novaData = $('#novaData').val();
+        const novaTarefa = $('#inserirNovadata').val();
+        const novoItem = $('<li></li>');
+    
+        const tarefaText = document.createTextNode(novaTarefa);
+        novoItem.append(tarefaText);
+        novoItem.text(`When:${novaData} - Task: ${novaTarefa}`);
+    
+        $('ul').append(novoItem);
+    
+        $(document).ready(function(){
+            $('li').click(function(){
+                $('this').css('text-decoration', 'line-through');
+                $('this').css('background-color', '#6ee24a')
+            });
+        });
+    });
+});
+
